f625d1865ab8073d05a06cbb2682fcb0becdcdc8
Daedalus <13687739+daedalus1215@users.noreply.github.com>
2025-08-11 20:10:56 -0400

    emotion-predicition-pipeline: use distilbert and roberta and compare the results.

--- emotion-predicition-pipeline/distilbert.py
+++ emotion-predicition-pipeline/distilbert.py
@@ -1,10 +1,13 @@
 from transformers import pipeline
+import pandas as pd
 
 nlp = pipeline(
-    # "sentiment-analysis",
     "text-classification",
     model="distilbert/distilbert-base-uncased-finetuned-sst-2-english",
     revision="714eb0f",   # or omit and let it use latest
     device_map="auto"     # will pick GPU if available
 )
-print(nlp("this is great"))
+
+result =  nlp("this is great")
+print(result)
+pd.DataFrame(result)
