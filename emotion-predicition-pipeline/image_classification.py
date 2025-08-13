d33db02e56f066b94cc1627dac3b94c6a216d9a9
Daedalus <13687739+daedalus1215@users.noreply.github.com>
2025-08-12 22:56:40 -0400

    image classification

---
+++ emotion-predicition-pipeline/image_classification.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import requests
+from transformers import pipeline
+
+
+
+url = "https://imgs.search.brave.com/fM5fe860LYWvURuki4Vj3MAu6_2Tpv67akybmFBpwYU/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/YXNwY2Eub3JnL3Np/dGVzL2RlZmF1bHQv/ZmlsZXMvY2F0LWNh/cmVfYWdncmVzc2lv/bi1pbi1jYXRzX21h/aW4taW1hZ2UuanBn"
+image = Image.open(requests.get(url, stream=True).raw)
+classifier = pipeline("image-classification")
+outputs = classifier(image)
+print(outputs)
