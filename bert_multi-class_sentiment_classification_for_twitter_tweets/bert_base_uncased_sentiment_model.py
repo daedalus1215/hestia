bea855e4f1af9a656cf2f6d32c4545b20ab944df
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2025-12-06 22:45:50 -0500

    .

--- bert_multi-class_sentiment_classification_for_twitter_tweets/bert_base_uncased_sentiment_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
-model_dir = './bert-base-uncased-serntiment-model'
-model = AutoModelForSequenceClassification.from_pretrained(model_dir)
-tok   = AutoTokenizer.from_pretrained(model_dir)
-classifier = pipeline("text-classification", model=model, tokenizer=tok)
-print(classifier(['I had a great time', 'My dog brings me happiness']))
--- random_transformers/image_classification/facial_emotional_recognition.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import requests
-from transformers import pipeline
-
-url = "https://imgs.search.brave.com/RLOqlKgjCPXo8hBngVheVIm_vP9zqwl8dMk-oj-KXuM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQx/MzM2ODU2NS9waG90/by9oYXBweS10cmVu/ZHktYW5kLWZ1bmt5/LWJsYWNrLW1hbi1s/b29raW5nLWNoZWVy/ZnVsLXdpdGgtYS1i/aWctc21pbGUtb24t/aGlzLWZhY2Utc3Rh/bmRpbmcuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPXpCZ3pn/NnJBMVNzQ1REaWQ5/WWExZU53OHhOTWRD/Sk4zWDVqRG4tVDZj/bFk9"
-image = Image.open(requests.get(url, stream=True).raw)
-classifier = pipeline("image-classification", model="Rajaram1996/FacialEmoRecog")
-outputs = classifier(image)
-print(outputs)
